209a426789f55e9b47ffc0fbe8792fe0d4d461a3
Ismail ZAHIR <ismailza407@gmail.com>
2024-10-02 14:22:43 +0100

    add initial models

---
+++ Models/Category.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace zstore.net.Models
+{
+  public class Category
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    public required string Type { get; set; }
+
+    public long? ParentCategoryId { get; set; }
+
+    [ForeignKey("ParentCategoryId")]
+    public Category? ParentCategory { get; set; }
+  }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace zstore.net.Models
+{
+  public class Product
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    [Range(0, Double.MaxValue)]
+    public required double Price { get; set; }
+
+    [Range(0, int.MaxValue)]
+    [Required]
+    public required int Quantity { get; set; }
+
+    [Required]
+    public required long CategoryId { get; set; }
+
+    [ForeignKey("CategoryId")]
+    public Category? Category { get; set; }
+
+    public string? ImageUrl { get; set; }
+  }
+}
