0db96e051c5897c3b0e5f1aae74623937d699e34
Ismail ZAHIR <ismailza407@gmail.com>
2024-10-02 14:23:06 +0100

    setup db context and compose file

---
+++ Data/ZStoreDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using zstore.net.Models;
+
+namespace zstore.net.Data
+{
+  public class ZStoreDbContext : DbContext
+  {
+    public ZStoreDbContext(DbContextOptions<ZStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+  }
+}
