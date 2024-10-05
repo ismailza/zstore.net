c5f3cf9ac782ef4fefc542590e940474aa4d46a0
Ismail ZAHIR <ismailza407@gmail.com>
2024-10-05 14:04:55 +0100

    add hero and categories to home page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using zstore.net.Data;
+using zstore.net.Models;
 
 namespace zstore.net.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ZStoreDbContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ZStoreDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+    public List<Category> Categories { get; set; } = [];
 
+    public async Task OnGetAsync()
+    {
+        Categories = await _context.Categories
+            .OrderBy(c => c.Name)
+            .ToListAsync();
     }
 }
