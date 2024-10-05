ab0c50652a8ac0272be06568a330adc525145228
Ismail ZAHIR <ismailza407@gmail.com>
2024-10-05 09:53:41 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './Pages/**/*.cshtml',
+    './Views/**/*.cshtml',
+    './wwwroot/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
